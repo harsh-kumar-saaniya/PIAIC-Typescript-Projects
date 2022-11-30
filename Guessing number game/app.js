c2d4762d4bc08c7ec6866c4e50315b9501343c63
harsh-kumar-saaniya <harshvithal1@gmail.com>
2022-12-01 02:51:00 +0500

    Added the functionality for making multiples rounds as per user guessed value

--- Guessing number game/app.js
+++ Guessing number game/app.js
@@ -1,23 +1,62 @@
 // GUESSING NUMBER GAME
 import inquirer from 'inquirer';
-// GENERATING THE RANDOME NUMBER
-let randNum = Math.floor(Math.random() * 10);
-console.log("value by system", randNum);
+// GENERATING THE RANDOM NUMBER
+// let randNum: number = Math.floor(Math.random() * 10);
 // GETTING THE NUMBER FROM USER
+var winner;
+var round = 0;
 const valueGetter = async () => {
+    let randNum = Math.floor(Math.random() * 10);
+    console.log("value by system", randNum);
     let userInput = await inquirer.prompt([{
             name: "userInput",
             type: 'number',
-            message: "Please Enter any number: "
+            message: "Guess any number between 0 to 9: "
         }]);
-    console.log(userInput.userInput);
     if (randNum === userInput.userInput) {
+        winner = true;
         console.log("Wow, you guessed the right number...!!!");
     }
     else {
+        winner = false;
         console.log("Opps... You guessed the wrong number try agin!");
     }
 };
-valueGetter();
-// CHECKING THE RANDOM NUMBER WITH THE USER INPUTTED NUMBER
+// MAKING THREE ROUNDS
+const againStarter = async () => {
+    do {
+        await valueGetter();
+        round++;
+        var again = await inquirer.prompt([
+            {
+                name: "nextLevel",
+                type: "input",
+                message: round === 3 ? 'Congratulation you win all the round best wishes' : (winner === true ? `You won the ${round} round, Are you ready for the next round y or n?` : "You lost the game, Best Luck!!!")
+            }
+        ]);
+    } while (winner === true && round < 3 && again.nextLevel == 'y' || again.nextLevel == "Y" || again.nextLevel == 'yes' || again.nextLevel == "YES");
+};
 // WINNING AND LOSE STATEMENT
+await againStarter();
+// NEED TO ADD THE FURTHER FUNCTIONALITY FOR IMPROVING THE CODE
+// const againStarter = async () => {
+//     await valueGetter()
+//     let round = 1;
+//     if (winner === true) {
+//         var again = await inquirer.prompt([
+//             {
+//                 name: "nextLevel",
+//                 type: "input",
+//                 message: "You won the first round, Are you ready for the second round y or n "
+//             }
+//         ])
+//         while (again.nextLevel == 'y' || again.nextLevel == 'yes' || again.nextLevel == 'Y' || again.nextLevel == 'YES' && winner === true && round === 2) {
+//             await valueGetter()
+//             round++;
+//         }
+//     }
+//     // else {
+//     //     console.log("You lost!!!")
+//     // }
+// }
+// await againStarter()
