c7c9dd2a01c354e0c634c9477fc0ccced7782030
harsh-kumar-saaniya <harshvithal1@gmail.com>
2023-01-23 01:40:56 +0500

    Started work on project need to create multiple scnerio

---
+++ Adventure Game/index.js
@@ -0,0 +1,10 @@
+"use strict";
+class Main {
+    constructor() {
+        // system objects
+        // Game Variable
+        this.enemies = ["Skeleton", "Zombie", "Warrior", "Assassin"];
+        this.maxEnemyHealth = 75;
+        this.enemyAttackDamage = 25;
+    }
+}
---
+++ Adventure Game/index.ts
@@ -0,0 +1,10 @@
+
+class Main {
+    // system objects
+
+    // Game Variable
+
+    enemies: string[] = ["Skeleton", "Zombie", "Warrior", "Assassin"];
+    maxEnemyHealth = 75;
+    enemyAttackDamage = 25;
+}
